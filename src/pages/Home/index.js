84cb1bf877455ad8961a8463f4cf67f40560dbbe
denisluciano30 <denis.lopes3012@gmail.com>
2020-06-09 17:02:32 -0300

    mudanças no card da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
 
 function CardGaragem() {
   return(
     <View style={styles.cardGaragem}>
-      <Text>Home!</Text>
+      <View style={ styles.containerImageGarage }>
+        <Image
+        source={require('../../assets/garagem.png')}
+        />
+      </View>
+      <View style={styles.containerInfoCard}>
+        <View style={styles.headerInfoCard}>
+          <Text>Garagem completa</Text>
+
+          <Text>s2</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,7 +23,10 @@ function CardGaragem() {
 
 function Home({ navigation }) {
   return (
-    <ScrollView style={styles.scrollViewHome}>
+    <ScrollView
+      style={styles.scrollViewHome}
+      showsVerticalScrollIndicator={false}
+    >
       <CardGaragem />
       <CardGaragem />
       <CardGaragem />
@@ -36,17 +50,39 @@ export default Home;
 
 const styles = StyleSheet.create({
   cardGaragem: {
-    flex: 0.5,
-    padding: 30,
+    flex: 1,
+    flexDirection:"row", //Indica a direção dos items, se é vertical ou horizontal
+    // justifyContent: "space-between",
+
+    paddingBottom: 15,
+    paddingTop: 15,
+    paddingLeft: 10,
+    paddingRight:10,
     backgroundColor: "#fff",
     marginTop: 5,
     marginLeft: 10,
     marginRight: 10,
     borderRadius: 5,
-    //android
     elevation: 5
   },
   scrollViewHome: {
     marginTop: 10,
-  }
+  },
+  containerInfoCard:{
+    flex:1,
+
+    backgroundColor: 'red',
+  },
+  containerImageGarage:{
+
+
+    // backgroundColor: 'red',
+  },
+  headerInfoCard:{
+    flexDirection:"row",
+    justifyContent: "space-between",
+    paddingTop: 5,
+    paddingRight: 5,
+    paddingLeft: 5.
+  },
 });
